eb9307b656649fc90bb79e1ca6ba60805fa27af3
pawsitive-dav <rhea.captain@gmail.com>
2025-01-06 16:42:17 +0700

    Update

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@ use Slim\Factory\AppFactory;
 use App\Database\Connection;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+use Psr\Http\Message\ResponseInterface as Response;
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -15,7 +16,16 @@ $app = AppFactory::create();
 
 Connection::initialize();
 
-$app->add(function (Request $request, RequestHandler $handler) {
+$app->add(function (Request $request, RequestHandler $handler): Response {
+    if ($request->getMethod() === 'OPTIONS') {
+        $response = new \Slim\Psr7\Response();
+        return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+            ->withStatus(200);
+    }
+
     $response = $handler->handle($request);
     return $response
         ->withHeader('Access-Control-Allow-Origin', '*')
